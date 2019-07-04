1c8f282c15b776d709c8d3bb2417e4c5701eab40
iSmash <nemeth.richo@gmail.com>
2019-07-04 18:54:33 +0100

    small test correcton on signing_user_up_spec.rb and make the test passed

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -9,9 +9,10 @@ RSpec.feature "Users sign_up" do
      click_link "Sign up"
      fill_in "Email", with: "john@example.com"
      fill_in "Password", with: "password"
+     fill_in "Password confirmation", with: "password"
      click_button "Sign up"
 
-     expect(page).to have_content("You have signed up successfully.")
+     expect(page).to have_content("Welcome! You have signed up successfully.")
 
    end
 end
