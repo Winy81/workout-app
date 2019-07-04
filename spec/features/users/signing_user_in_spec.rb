f4fab349e189e8317283596be6e86181d5feaba2
iSmash <nemeth.richo@gmail.com>
2019-07-04 19:11:47 +0100

    setup test signing_user_in_spec.rb for case as 'Sign up successfully'.

---
+++ spec/features/users/signing_user_in_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "User sign_in" do
+
+  before do
+  	@john = User.create!(email: "john@example.com", password: "password")
+  end
+
+  scenario "Sign up successfully" do 
+
+  	visit "/"
+
+  	click_link "Sign in"
+  	fill_in "Email", with: "@john.email"
+    fill_in "Password", with: "@john.password"
+    click_button "Sign in"
+
+    expect(page).to have_content("Welcome! You have signed in successfully.")
+    expect(page).to have_content("Signed in as #{@john.email}")
+
+  end
+
+end 
