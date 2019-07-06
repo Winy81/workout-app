eeee020a528dded2d528995e48db007fed92f36f
iSmash <nemeth.richo@gmail.com>
2019-07-06 11:53:34 +0100

    small change on signing_user_in_spec.rb#Sign in successfully and make the test pass

--- spec/features/users/signing_user_in_spec.rb
+++ spec/features/users/signing_user_in_spec.rb
@@ -6,16 +6,16 @@ RSpec.feature "User sign_in" do
   	@john = User.create!(email: "john@example.com", password: "password")
   end
 
-  scenario "Sign up successfully" do 
+  scenario "Sign in successfully" do 
 
   	visit "/"
 
   	click_link "Sign in"
-  	fill_in "Email", with: "@john.email"
-    fill_in "Password", with: "@john.password"
-    click_button "Sign in"
+  	fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    click_button "Log in"
 
-    expect(page).to have_content("Welcome! You have signed in successfully.")
+    expect(page).to have_content("Signed in successfully.")
     expect(page).to have_content("Signed in as #{@john.email}")
 
   end
