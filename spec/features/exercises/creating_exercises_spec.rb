e7efb372192790538f454dfe22342af768f13c33
iSmash <nemeth.richo@gmail.com>
2019-07-10 16:47:52 +0100

    expand test of creating_exercises_spec.rb#with valid details expectations

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -19,6 +19,12 @@ RSpec.feature "Creating exercise" do
   	fill_in "Workout Details", with: "Weight lifting"
   	fill_in "Activity date", with: "2019-07-03"
   	click_button "Create Exercise"
+
+  	expect(page).to have_content("Exercise has been created")
+
+  	exercise = Exercise.last
+  	expect(current_path).to eq(user_exercise_path(@john,exercise))
+  	expect(exercise.user_id).to eq(@john.id)
   end
 
 end
