1e5b762a473ccfde130f213636dbd72964df5397
iSmash <nemeth.richo@gmail.com>
2019-07-10 15:16:35 +0100

    creat test creating_exercises_spec.rb#with invalid details

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -7,4 +7,18 @@ RSpec.feature "Creating exercise" do
   	login_as(@john)
   end
 
+  scenario "with valid input" do 
+
+  	visit "/"
+
+  	click_link "My Lounge"
+  	click_link "New Workout"
+  	expect(page).to have_button("Back")
+
+  	fill_in "Duration", with: 70
+  	fill_in "Workout Details", with: "Weight lifting"
+  	fill_in "Activity date", with: "2019-07-03"
+  	click_button "Create Exercise"
+  end
+
 end
