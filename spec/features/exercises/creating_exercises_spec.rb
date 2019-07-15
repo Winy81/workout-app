3ae8032ed52667b8e636d90ef4d7e3a1c29aca35
iSmash <nemeth.richo@gmail.com>
2019-07-15 16:24:35 +0100

    exercise#new form has been setup

--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -13,9 +13,9 @@ RSpec.feature "Creating exercise" do
 
   	click_link "My Lounge"
   	click_link "New Workout"
-  	expect(page).to have_button("Back")
+  	expect(page).to have_link("Back")
 
-  	fill_in "Duration", with: 70
+  	fill_in "Duration (min)", with: 70
   	fill_in "Workout Details", with: "Weight lifting"
   	fill_in "Activity date", with: "2019-07-03"
   	click_button "Create Exercise"
