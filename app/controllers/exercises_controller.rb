8df2d217323156269095c200ad8c346294fc50ee
iSmash <nemeth.richo@gmail.com>
2019-07-17 13:45:17 +0100

    exercises#create

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,11 +2,21 @@ class ExercisesController < ApplicationController
 
   def index
 
-  
   end
 
   def new
   	@exercise = current_user.exercises.new
+  end
+
+  def create
+  	@exercise = current_user.exercises.new
+  	if @exercise.save
+  	  flash[:notice] = "Exercise has been created"
+  	  redirect_to user_exercise_path(current_user, @exercise) # or redirect_to [current_user, @exercise]  	  
+  	else
+  	  flash[:alert] = "Exercise has not been created"
+  	  render :new
+  	end
   end
 
 end
