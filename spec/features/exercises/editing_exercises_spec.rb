d3e15f76273a5f6b35eaf489b74801c70b3332e0
iSmash <nemeth.richo@gmail.com>
2019-07-22 16:41:55 +0100

    test editing_exercises_sepc.rb#with valid details

--- spec/features/exercises/editing_exercises_spec.rb
+++ spec/features/exercises/editing_exercises_spec.rb
@@ -5,9 +5,28 @@ RSpec.feature "Editing exercise" do
   before do 
   	@john = User.create!(email: "john@example.com", password: "password")
   	@johns_exercise = @john.exercises.create!(duration_in_min: 240,
-  											  workout:"lorem_ipsum"
+  											  workout:"lorem_ipsum",
   											  workout_date: 3.day.ago)
   	login_as(@john)
+  end
+
+  scenario "with valid data succeeds" do 
+
+  	visit "/"
+
+  	click_link "My Lounge"
+
+  	path = "users/#{@john.id}/exercises/@johns_exercise/edit"
+  	link = "a[href=\'#{path}\']"
+  	find(link).click_link
+
+  	    fill_in "Duration", with: 45
+    click_button "Update Exercise"
+    
+    expect(page).to have_content("Exercise has been updated")
+    expect(page).to have_content(45)
+    expect(page).not_to have_content(240)
+
   end
 
 end
