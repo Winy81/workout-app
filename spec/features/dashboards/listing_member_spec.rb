29af4b9bf0e1b5ff0d1b00bac76d9d66cba5975f
iSmash <nemeth.richo@gmail.com>
2019-07-25 16:19:28 +0100

    listing_memeber_spec_rb#before

---
+++ spec/features/dashboards/listing_member_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.feature "Listing members" do 
+
+  before do 
+  	@john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+  	@jane = User.create(first_name: "Jane", last_name: "Doe", email: "jane@example.com", password: "password")
+  end
+
+end
