ec7209a10d4500f107eca5b5736761693094c2f8
iSmash <nemeth.richo@gmail.com>
2019-07-25 10:10:28 +0100

    exercises#update

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -28,6 +28,17 @@ class ExercisesController < ApplicationController
   	@exercise = current_user.exercises.find(params[:id])
   end
 
+  def update
+  	@exercise = current_user.exercises.find(params[:id])
+  	if @exercise.update(exercise_params)
+  	  flash[:notice] = "Exercise has been updated"
+  	  redirect_to :action => "show", :id => @exercise
+  	else
+  	  flash[:alert] = "Exercise has not been updated"
+  	  render :edit
+  	end
+  end
+
   private
 
   def exercise_params
