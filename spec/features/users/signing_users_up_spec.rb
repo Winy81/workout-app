8ed3db35df8352e7395250e5116509fdc5182ca9
iSmash <nemeth.richo@gmail.com>
2019-07-26 17:21:41 +0100

    test expandation on signing_user_up_spec.rb# with invalid details

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -30,14 +30,16 @@ RSpec.feature "Users sign_up" do
      visit "/"
 
      click_link "Sign up"
-     fill_in "First name", with: "John"
-     fill_in "Last name", with: "Doe"
+     fill_in "First name", with: ""
+     fill_in "Last name", with: ""
      fill_in "Email", with: "john@example.com"
      fill_in "Password", with: ""
      fill_in "Password confirmation", with: ""
      click_button "Sign up"
 
      expect(page).to have_content("Password can't be blank")
+     expect(page).to have_content("First name can't be blank")
+     expect(page).to have_content("Last name can't be blank")
      expect(page).to have_link("Sign up")
      expect(page).to have_link("Sign in")
 
