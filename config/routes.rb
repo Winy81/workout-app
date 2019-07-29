b92a5cd129dedfb144c93ac51b263b2ee09bd97b
iSmash <nemeth.richo@gmail.com>
2019-07-29 13:12:44 +0100

    route has been setup for the search customer action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   
   devise_for :users, :controllers => {:registrations => "registrations"}
-  
+
   root to: "dashboards#index"
 
   resources :users do
     resources :exercises
+  end
+
+  resources :dashboards, only:[:index] do 
+  	collection do 
+  	  post :search, to: 'dashboards#search'
+  	end
   end
 
 end
