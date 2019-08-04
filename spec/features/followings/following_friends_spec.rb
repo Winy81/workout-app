7d8f858ce107c9cefd5bc31bc4bf68971367aecc
iSmash <nemeth.richo@gmail.com>
2019-08-04 13:13:10 +0100

    setup link for Follow a user

--- spec/features/followings/following_friends_spec.rb
+++ spec/features/followings/following_friends_spec.rb
@@ -5,8 +5,25 @@ RSpec.feature "Following Friends" do
   before do 
   	@john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
   	@jane = User.create(first_name: "Jane", last_name: "Doe", email: "jane@example.com", password: "password")
+
+    login_as(@john)
+
   end
 
-  login_as(@john)
+  scenario "if signed in" do
+    visit "/"
 
+    expect(page).to have_content(@john.full_name)
+    expect(page).to have_content(@jane.full_name)
+
+    href = "/friendships?friend_id=#{@john.id}"
+    expect(page).not_to have_link("Follow", :href => href)
+
+    link = "a[href='/friendships?friend_id=#{@peter.id}']"
+    find(link).click
+
+    href = "/friendships?friend_id=#{@peter.id}"
+    expect(page).not_to have_link("Follow", :href => href)  	
+
+  end
 end
