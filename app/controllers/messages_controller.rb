e75335582dabfc6d026ac44d4bf276ebaa98e7a9
iSmash <nemeth.richo@gmail.com>
2019-08-17 17:35:33 +0100

    messages_controller#create action has been done wiht hite filter

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,19 @@
 class MessagesController < ApplicationController
 
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	def create
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.room = current_room
+    
+    @message.save
+    redirect_to user_exercises_path(current_user)
+  end
 
-	end
+  private
+  
+  def message_params
+    params.require(:message).permit(:body)
+  end
 
 end
