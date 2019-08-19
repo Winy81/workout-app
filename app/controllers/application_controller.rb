b7342a4f4ebe299ebf9f92c3a3f34bfdb8ad760c
iSmash <nemeth.richo@gmail.com>
2019-08-19 16:11:30 +0100

    protect_from_forgery with: :exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  
+  protect_from_forgery with: :exception
 
   private
 
@@ -7,5 +9,5 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_room
-  
+
 end
