2d6b370ec41c4440a6954bd9050cc3343c919bcd
iSmash <nemeth.richo@gmail.com>
2019-08-21 11:58:14 +0100

    checkOut method in the messages_channel.rb

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -11,5 +11,9 @@ class MessagesChannel < ApplicationCable::Channel
   	room_id = "#{data['room_id']}"
   	stream_from "messages_room_#{room_id}"
   end
-  
+
+  def checkOut
+  	stop_all_streams
+  end
+
 end
