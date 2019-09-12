0af42d7722b823a5b344ed88ad5480d4888dcbd1
iSmash <nemeth.richo@gmail.com>
2019-09-12 15:24:40 +0100

    30 random exercise has been created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'date'
+
+
 User.create!(
   first_name: "John", 
   last_name: "Doe", 
@@ -10,7 +13,7 @@ puts "John as user has been created"
 User.create!(
   first_name: "Jane", 
   last_name: "Doe", 
-  email: "henry@example.com", 
+  email: "jane@example.com", 
   password: "password"
 )
 
@@ -27,13 +30,26 @@ puts "Henry as user has been created"
 
 
 
-
 10.times do |user|
-	User.create!(
-	  first_name: "#{user}Test", 
-	  last_name: "User", 
-	  email: "#{user}TestUser@example.com", 
-	  password: "password"
+  User.create!(
+    first_name: "#{user}Test", 
+    last_name: "User", 
+    email: "#{user}TestUser@example.com", 
+    password: "password"
+  )
 end
 
 puts "10 random User has been created"
+
+
+30.times do |exercise|
+  Exercise.create!(
+  	duration_in_min: rand(25..450).to_s,
+  	workout: "This is duty of #{exercise}",
+  	workout_date: Date.today.next_day(rand(1..20)),
+  	user_id: rand(1..3)
+  )
+end
+
+puts "30 random exercises has been created, duration_in_min workout date is random and the owner User is one of the main 3 users."
+
